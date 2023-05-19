81f5e49f46e377e07cb80587649b7307cc6dce7b
Khaled-Waled <khaledw.hamed001@gmail.com>
2023-05-19 21:58:33 +0300

    added error to test CI

--- RegistrationSys_StudentUI/src/Main.java
+++ RegistrationSys_StudentUI/src/Main.java
@@ -4,6 +4,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 import java.util.stream.Collectors;
 
+testing error
 public class Main
 {
     public static void enterBatchStud() throws IOException
